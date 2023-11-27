31bf262169796c26a96b527361772744c55fd046
parshu-8203 <parshu.yerranagu@gmail.com>
2023-11-27 08:18:57 +0530

    integrated authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { FaList } from 'react-icons/fa';
-import { BrowserRouter as Router,Route,Routes, useLocation, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 import Content from './components/Content/Content';
+import { Login } from './components/Home/Login';
+import About from './components/Home/About';
+import Home from "./components/Home/Home";
 
 const App = () => {
-  const location=useLocation();
-  var path=location.pathname;
-   path=path.slice(1,)
+  const [isAuthenticated, setAuthentication]  = useState(localStorage.getItem('jwtToken'));
+
+  const location = useLocation();
+  var path = location.pathname;
+  path = path.slice(1,)
   const [showSidebar, setShowSidebar] = useState(window.innerWidth >= 768); // Show sidebar by default for widths >= 768px
   const [selectedNavItem, setSelectedNavItem] = useState(path);
 
@@ -25,24 +30,28 @@ const App = () => {
     return () => {
       window.removeEventListener('resize', handleResize);
 
-      
+
     };
   }, []);
 
   return (
-   
+    isAuthenticated ? (
+
       <div className="app">
         <button className="hamburger" onClick={handleHamburgerClick}>
           <FaList />
         </button>
         <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} setSelectedNavItem={setSelectedNavItem} />
         <Routes>
-              <Route path={selectedNavItem} element={<Content selectedNavItem={selectedNavItem} />}>    
-    
-        </Route>
+          <Route path={selectedNavItem} element={<Content selectedNavItem={selectedNavItem} />}>
+
+          </Route>
         </Routes>
       </div>
-   
+    ) : (
+      <About setAuthentication={setAuthentication}/>
+    )
+
   );
 };
 
--- frontend/src/components/Home/Login.jsx
+++ frontend/src/components/Home/Login.jsx
@@ -3,59 +3,92 @@ import './module.home.css';
 import { FcGoogle } from "react-icons/fc";
 import { FaRegEye } from "react-icons/fa";
 import { useState } from 'react';
+import api from '../../apis/axiosConfig';
+export const Login = ({ setAuthentication }) => {
+  const [logindetails, setlogindetails] = useState({
+    email: "",
+    password: ""
+  });
+ 
 
-export const Login = () => {
-  const [logindetails,setlogindetails]=useState({
-    email:"",
-    password:""
-  })
+  const handleChange = (e) => {
+    setlogindetails({ ...logindetails, [e.target.name]: e.target.value });
+  };
 
-const handleChange=e=>{
-  setlogindetails({...logindetails,[e.target.name]:e.target.value});
-}
+  const handlesubmit = async(e) => {
+    e.preventDefault();
 
-const handlesubmit=e=>{
-  e.preventDefault();
-  console.log(logindetails)
-  setlogindetails({
-    email:"",
-    password:""
-  })
-}
+    try {
+      // Make a POST request to your login API endpoint
+      const response = await api.post('/login',  {
+        email: logindetails.email,
+        password: logindetails.password,
+      });
 
-const handleshowpassword=()=>{
-  var x =document.getElementById('password');
-  if(x.type==="password"){
-    x.type="text"
-  }
-  else{
-    x.type="password"
-  }
-}
+      // Handle the response from the server
+      console.log(response.data); // Log the server response
+      const jwtToken = response.data.jwtToken;
 
+      // Save JWT token and user details to localStorage
+      localStorage.setItem('jwtToken', jwtToken);
+      localStorage.setItem('userDetails', JSON.stringify(response.data.userDetails));
+
+      // Assuming the server returns a JWT token upon successful login
+      // You may want to save the token in the client's state or localStorage for further authentication
+
+      // For simplicity, let's assume setting authentication status to true upon successful login
+      setAuthentication(true);
+
+      // Redirect to another page if needed
+      // For example, using react-router-dom
+      // history.push('/dashboard');
+    } catch (error) {
+      // Handle error responses from the server
+      console.error('Login failed:', error.response.data);
+
+      // For simplicity, setting authentication status to false upon failed login
+      setAuthentication(false);
+    }
+
+    // Reset login details
+    setlogindetails({
+      email: '',
+      password: '',
+    });
+  };
+
+
+  const handleshowpassword = () => {
+    var x = document.getElementById('password');
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
   return (
     //Login UI Code
-    <div className=' p-3 text-center'>
+    <div className='p-3 text-center'>
       <div className='d-flex justify-content-center'>
         <form onSubmit={handlesubmit} autoComplete='on'>
-        <h2 className='pt-2'>Login here</h2><br/>
-        <label className='login-label pb-1'> Email</label>
-        <input type="email" placeholder='Email' name="email" value={logindetails.email} onChange={handleChange} className='p-2 text-start login-input shadow' required /><br/><br/>
-        <label className='login-label pb-1'> Password </label>
-        <input type="password" id="password" placeholder='password' name="password" value={logindetails.password} onChange={handleChange} className='p-2 text-start login-input shadow' required /><br/>
-        <h6 className='d-flex text-center pt-2'> <input type='checkbox' onClick={handleshowpassword}/> &nbsp; Show password</h6>
-        <input type="submit" className='btn bg text-center mt-4 input-button shadow' value="Login" />
-        
+          <h2 className='pt-2'>Login here</h2><br />
+          <label className='login-label pb-1'> Email</label>
+          <input type="email" placeholder='Email' name="email" value={logindetails.email} onChange={handleChange} className='p-2 text-start login-input shadow' required /><br /><br />
+          <label className='login-label pb-1'> Password </label>
+          <input type="password" id="password" placeholder='password' name="password" value={logindetails.password} onChange={handleChange} className='p-2 text-start login-input shadow' required /><br />
+          <h6 className='d-flex text-center pt-2'> <input type='checkbox' onClick={handleshowpassword} /> &nbsp; Show password</h6>
+          <input type="submit" className='btn bg text-center mt-4 input-button shadow' value="Login" />
+
         </form>
 
       </div>
-      <br/>
+      <br />
       <p className='text-center'>------ or -------</p>
-      
+
       {/* Google authentication in login */}
       <a href="#" className="">
-          <button className='btn text-center input-button bg-light shadow'><FcGoogle size={30}/> Login with Google</button>
-        </a>
+        <button className='btn text-center input-button bg-light shadow'><FcGoogle size={30} /> Login with Google</button>
+      </a>
     </div>
   )
 }
